d35cf3d8951c36cd4c1092e45d736c6ae1bd0284
kumneger Getiye <159164899+assumaqn@users.noreply.github.com>
2025-07-25 20:09:18 +0000

    Add reusable Card component and use it on Home page

--- components/common/Card.tsx
+++ components/common/Card.tsx
@@ -1,10 +1,3 @@
-export default function HomePage() {
-  return (
-    <main className="p-8">
-      <h1 className="text-3xl font-bold text-blue-600">Welcome to the Home Page</h1>
-    </main>
-  )
-}
 import { CardProps } from '@/interfaces'
 
 const Card = ({ title, content }: CardProps) => {
