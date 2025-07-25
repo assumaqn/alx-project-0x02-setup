83f3559a042aaf827af9b9ecbcf4b5bfd1499a9d
kumneger Getiye <159164899+assumaqn@users.noreply.github.com>
2025-07-25 20:14:10 +0000

    Add reusable Button component and use it on About page

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -6,3 +6,8 @@ export interface CardProps {
   title: string
   content: string
 }
+export interface ButtonProps {
+  label: string
+  size: 'small' | 'medium' | 'large'
+  shape: 'rounded-sm' | 'rounded-md' | 'rounded-full'
+}
